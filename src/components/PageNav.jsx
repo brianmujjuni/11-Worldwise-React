79d59050ba542454485c9bd60cee7ae1d9122ca0
brianmujjuni <brianmujjuni@engineer.com>
2024-03-27 15:27:51 +0300

    import logo component and display it

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,15 +1,21 @@
-import {  NavLink } from "react-router-dom";
-import styles from  "./PageNav.module.css"
-
+import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 export default function PageNav() {
   return (
     <nav className={styles.nav}>
-        <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/pricing">Pricing</NavLink></li>
-            <li><NavLink to="/product">Product</NavLink></li>
-             <li><NavLink to="/product">Product</NavLink></li>
-        </ul>
+      <Logo />
+      <ul>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+      </ul>
     </nav>
-  )
+  );
 }
