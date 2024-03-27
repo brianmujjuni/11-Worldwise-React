2bfcefa73d877cbc2cd6b46c1bb3732f08ee54f4
brianmujjuni <brianmujjuni@engineer.com>
2024-03-27 14:42:54 +0300

    add global css

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,14 @@
 
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppLayout from "./AppLayout";
 
 export default function Homepage() {
   return (
     <div>
         <PageNav/>
         <h1>Home Page</h1>
+        <Link to="/app" element={<AppLayout/>}>App</Link>
     </div>
   )
 }
