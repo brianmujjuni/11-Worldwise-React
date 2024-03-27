373e134fbfebd2d055ede40608381cdf7cf53fa9
brianmujjuni <brianmujjuni@engineer.com>
2024-03-27 17:30:39 +0300

    include index route in app route

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,6 +10,7 @@ export default function Sidebar() {
         <AppNav/>
 
         <Outlet/>
+        
         <footer className={styles.footer}>
             <p className={styles.copyright}>
                 &copy; Copyright {new Date().getFullYear()} by Automex Technologies
