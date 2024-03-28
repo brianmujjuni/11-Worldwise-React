5c771b8a327ae083c19982e4bfecb970c3e52c16
brianmujjuni <brianmujjuni@engineer.com>
2024-03-28 11:42:52 +0300

    fully import and use useParam hook

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="/app" element={<AppLayout />}>
+        <Route path="app" element={<AppLayout />}>
           <Route index element={<CityList cities={cities} isLoading={isLoading}/>} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
           <Route path="cities/:id" element={<City/>}/>
