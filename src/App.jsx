83919d9fe87283bc6f5f08d3a65ef8919e3be338
brianmujjuni <brianmujjuni@engineer.com>
2024-03-28 12:29:23 +0300

    useNavigate to link to form path in map component

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from './components/City'
-import { Form } from "./components/Form";
+import  Form  from "./components/Form";
 
 const BASE_URL = 'http://localhost:8000'
 
