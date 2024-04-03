53db7ab0884fe1d0b9e9434febd865a1492ff244
brianmujjuni <brianmujjuni@engineer.com>
2024-04-03 15:29:36 +0300

    remove props in app component

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ export default function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
           <Route index element={<Navigate replace to='cities'/>} />
-          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
+          <Route path="cities" element={<CityList />} />
           <Route path="cities/:id" element={<City/>}/>
-          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
+          <Route path="countries" element={<CountryList />} />
           <Route path="form" element={<Form/>} />
         </Route>
         <Route path="/login" element={<Login />} />
--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -1,26 +1,25 @@
-import { createContext,useState,useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 
-const BASE_URL = 'http://localhost:8000'
-const CitiesContext = createContext()
+const BASE_URL = "http://localhost:8000";
+const CitiesContext = createContext();
 
-function CitiesProvider({children}){
-    const [cities,setCities] = useState([])
-const [isLoading,setIsLoading] = useState(false)
-useEffect(()=>{
-  
- async function fetchCities(){
-  try {
-    setIsLoading(true)
-    const res = await fetch(`${BASE_URL}/cities`);
-    const data =  await res.json()
-    setCities(data)
-  } catch (error) {
-    console.log(error)
-    //alert('there was a problem loading data...')
-  }finally{
-    setIsLoading(false)
-  }
- }
- fetchCities()
-},[])
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        console.log(error);
+        //alert('there was a problem loading data...')
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
 }
