e13faa2b265f98a265a4ffed610263557b59cea9
brianmujjuni <brianmujjuni@engineer.com>
2024-04-03 15:25:52 +0300

    move cities state to citiesContext

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,7 @@ import  Form  from "./components/Form";
 const BASE_URL = 'http://localhost:8000'
 
 export default function App() {
-  const [cities,setCities] = useState([])
-  const [isLoading,setIsLoading] = useState(false)
 
-  useEffect(()=>{
-    
-   async function fetchCities(){
-    try {
-      setIsLoading(true)
-      const res = await fetch(`${BASE_URL}/cities`);
-      const data =  await res.json()
-      setCities(data)
-    } catch (error) {
-      console.log(error)
-      //alert('there was a problem loading data...')
-    }finally{
-      setIsLoading(false)
-    }
-   }
-   fetchCities()
-  },[])
   return (
     <BrowserRouter>
       <Routes>
