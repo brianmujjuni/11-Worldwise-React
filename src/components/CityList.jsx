58def3c98dc2eb03b1fcd82cf8522bcc82d01a8e
brianmujjuni <brianmujjuni@engineer.com>
2024-04-03 15:43:12 +0300

    consume the cities context in CityList

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,14 +1,21 @@
-import Spinner from "./Spinner"
-import styles from './CityList.module.css'
-import CityItem from "./CityItem"
-import Message from "./Message"
-export default function CityList({cities,isLoading}) {
-    if(isLoading) return <Spinner/>
-    if(!cities.length) return <Message message="Add your first city by clicking on a city on the map"/>
+import Spinner from "./Spinner";
+import styles from "./CityList.module.css";
+import CityItem from "./CityItem";
+import Message from "./Message";
+import { useCities } from "../context/CitiesContext";
+export default function CityList() {
+  const { cities, isLoading } = useCities();
+  
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
   return (
-    
-   <ul className={styles.cityList}>
-    {cities.map(city => <CityItem city={city} key={city.id}/>)}
-   </ul>
-  )
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
