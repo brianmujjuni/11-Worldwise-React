a0d9934de1085d6711bc2967e62fac9ca7586b1c
brianmujjuni <brianmujjuni@engineer.com>
2024-04-04 11:16:47 +0300

    add useEffect to update map positions

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,24 +1,28 @@
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import styles from "./Map.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCities } from "../context/CitiesContext";
-export default function Map() {
+function Map() {
   const navigate = useNavigate();
   const { cities } = useCities();
 
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const maplat = searchParams.get("lat");
+  const maplng = searchParams.get("lng");
+
+  useEffect(() => {
+    if (maplat && maplat) setMapPosition([maplat, maplng]);
+  }, [maplat, maplng]);
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <MapContainer
         className={styles.map}
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
       >
         <TileLayer
@@ -36,7 +40,16 @@ export default function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
       </MapContainer>
     </div>
   );
 }
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+export default Map;
