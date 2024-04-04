5344afe9149f6a6b5821135344b991f9ba3d4756
brianmujjuni <brianmujjuni@engineer.com>
2024-04-04 09:39:27 +0300

    create backButton component and use it

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import styles from './CityItem.module.css'
+import styles from "./CityItem.module.css";
+import { useCities } from "../context/CitiesContext";
 
-const formatDate =(date)=> new Intl.DateTimeFormat("en",{
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
     day: "numeric",
     month: "long",
     year: "numeric",
-}).format(new Date(date))
+  }).format(new Date(date));
 
-export default function CityItem({city}) {
-    const {cityName, emoji , date,id,position} = city
+export default function CityItem({ city }) {
+  const { cityName, emoji, date, id, position } = city;
+  const { currentCity } = useCities();
   return (
-    <li >
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+    <li>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
         <button className={styles.deleteBtn}>&times;</button>
-        </Link>
+      </Link>
     </li>
-  )
+  );
 }
