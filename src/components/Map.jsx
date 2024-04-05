7ed1722d3180d70550a5494d51885ed33f3582de
brianmujjuni <brianmujjuni@engineer.com>
2024-04-05 09:32:57 +0300

    create useUrlPosition custom hook

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,25 +11,37 @@ import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
 import { useCities } from "../context/CitiesContext";
 import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams, setSearchParams] = useSearchParams();
+
   const {
-    isLoadin: isLoadingPosition,
+    isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
 
-  const maplat = searchParams.get("lat");
-  const maplng = searchParams.get("lng");
+  
+ const [maplat,maplng] =  useUrlPosition()
 
   useEffect(() => {
-    if (maplat && maplat) setMapPosition([maplat, maplng]);
+    if (maplat && maplng) setMapPosition([maplat, maplng]);
   }, [maplat, maplng]);
+
+  useEffect(() => {
+    if (geolocationPosition)
+      setMapPosition([geolocationPosition.lng, geolocationPosition.lat]);
+  }, [geolocationPosition]);
 
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your postion"}
+        </Button>
+      )}
       <MapContainer
         className={styles.map}
         center={mapPosition}
@@ -37,7 +49,7 @@ function Map() {
         scrollWheelZoom={true}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">Automex</a> Technologies'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         {cities.map((city) => (
