da8611e98e888773f23bc4a30d6202fe3ade2120
brianmujjuni <brianmujjuni@engineer.com>
2024-04-06 15:36:06 +0300

    create login logic and logout

--- src/context/FakeAuthContext.jsx
+++ src/context/FakeAuthContext.jsx
@@ -16,15 +16,28 @@ function reducer(state, action) {
   }
 }
 
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
 function AuthProvider({ children }) {
   const [{ user, isAuthenticated }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
-  function login(email, password) {}
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+    }
+  }
 
-  function logout() {}
+  function logout() {
+    dispatch({ type: "logout" });
+  }
 
   return (
     <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
@@ -39,3 +52,5 @@ function useAuth() {
     throw new Error("AuthContext was used outside AuthProvider");
   }
 }
+
+export { AuthProvider, useAuth };
