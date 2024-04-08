25ba71c28323b6486b12386764e910c15f1eea14
brianmujjuni <brianmujjuni@engineer.com>
2024-04-08 11:35:42 +0300

    create protect component

---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useAuth } from "../context/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/");
+    }
+  }, [isAuthenticated, navigate]);
+  return children;
+}
